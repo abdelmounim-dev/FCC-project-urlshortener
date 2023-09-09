a684d6cf0c21cdbb18b75923dd5e35728a606763
abdelmounim-dev <77006851+abdelmounim-dev@users.noreply.github.com>
2023-09-09 10:36:23 +0000

    finished the project

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +16,43 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const urls = []
+const isUrl = (url) => {
+  if (typeof url !== 'string') return false 
+  let expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+  return url.match(expression);
+}
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:index', function(req, res) {
+  let index = req.params.index;
+
+  if(!isUrl(urls[index]))  res.send({ error: 'invalid url' });
+
+  res.redirect(urls[index]);
 });
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+
+  if(!isUrl(url)) {res.send({ error: 'invalid url' })
+  return;
+}
+
+  let index = urls.indexOf(url)
+
+  if (index === -1) {urls.push(url);
+    index = urls.length - 1
+  }
+
+  console.log(urls)
+
+  res.send({
+    original_url: url,
+    short_url: index
+  })
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
